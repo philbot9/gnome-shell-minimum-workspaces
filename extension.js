e6296e172e4b256a9a253254009178dd70f58c33
Philip Klostermann <philip.klostermann@gmail.com>
2014-05-08 22:18:20 +0100

    Initial commit

---
+++ extension.js
@@ -0,0 +1,53 @@
+// -*- mode: js2; indent-tabs-mode: nil; js2-basic-offset: 4 -*-
+
+const Meta = imports.gi.Meta;
+const Shell = imports.gi.Shell;
+const Main = imports.ui.main;
+
+const ExtensionUtils = imports.misc.extensionUtils;
+const Me = ExtensionUtils.getCurrentExtension();
+const Convenience = Me.imports.convenience;
+
+
+
+function set_fixed_workspaces(number) {
+    let min_workspaces = number;
+    let num_workspaces = global.screen.n_workspaces;
+    
+    //first make all workspaces non-persistent
+    for(let i = num_workspaces-1; i > 0; i--) {
+        global.screen.get_workspace_by_index(i)._keepAliveId = false;
+    }
+
+    //if we have less than the minimum workspaces create new ones and make them persistent
+    if(num_workspaces < min_workspaces) {
+        for(let i = num_workspaces; i < min_workspaces; i++) {
+            global.screen.append_new_workspace(false, 0);
+            global.screen.get_workspace_by_index(global.screen.n_workspaces - 1)._keepAliveId = true;    
+        }
+    } 
+    //if we already have enough workspaces make the first ones persistent
+    else {
+        for(let i = 0; i < min_workspaces-1; i++) {
+            global.screen.get_workspace_by_index(i)._keepAliveId = true;
+        }
+    }
+}
+
+function init() {
+    if (Meta.prefs_get_dynamic_workspaces()) {
+        this._schema = Convenience.getSettings();
+        set_fixed_workspaces(this._schema.get_int('minworkspaces'));
+    }
+}
+
+function enable() {
+    if (Meta.prefs_get_dynamic_workspaces()) {
+        this._schema = Convenience.getSettings();
+        set_fixed_workspaces(this._schema.get_int('minworkspaces'));
+    }
+}
+
+function disable() {
+    set_fixed_workspaces(0);
+}
---
+++ prefs.js
@@ -0,0 +1,61 @@
+/* -*- mode: js; js-basic-offset: 4; indent-tabs-mode: tabs -*- */
+
+const Gtk = imports.gi.Gtk;
+const Convenience = imports.misc.extensionUtils.getCurrentExtension().imports.convenience;
+const Lang = imports.lang;
+
+function init(){}
+
+function buildPrefsWidget() {
+    let settings = Convenience.getSettings();
+    
+    let frame = new Gtk.Box({
+        orientation: Gtk.Orientation.VERTICAL,
+        border_width: 10
+    });
+
+    let hbox = new Gtk.Box({
+        orientation: Gtk.Orientation.HORIZONTAL
+    });
+
+    let vbox = new Gtk.Box({
+        orientation: Gtk.Orientation.VERTICAL,
+        margin: 20,
+        margin_top: 10
+    });
+
+    let settingLabel = new Gtk.Label({
+        label: "Minimum number of Workspaces:",
+        xalign: 0
+    });
+
+
+    let noWorkspaces = new Gtk.SpinButton({xalign: 0});
+            //animationTime.set_sensitive(true);
+            noWorkspaces.set_range(1, 32);
+            noWorkspaces.set_value(settings.get_int('minworkspaces'));
+            noWorkspaces.set_increments(1, 1);
+            
+            noWorkspaces.connect('value-changed', function(button){
+                let s = button.get_value_as_int();
+                settings.set_int('minworkspaces', s);
+            });
+    
+    settingLabel.set_tooltip_text("Define the minimum number of Workspaces.");
+    noWorkspaces.set_tooltip_text("Define the minimum number of Workspaces.");
+
+    let  restartGnomeInfo = new Gtk.Label({label: "<i>The extension needs to be disabled and re-enabled for changes to take effect.</i>", use_markup: true, xalign: 2,hexpand:true});
+
+    hbox.pack_start(settingLabel, true, true, 0);
+    hbox.add(noWorkspaces);
+    
+    vbox.add(hbox);
+
+
+    frame.add(vbox);
+    frame.add(restartGnomeInfo);
+    frame.show_all();
+    return frame;
+}
+
+
